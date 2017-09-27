bd2a5930ada42d8d2b1b37c64f4e7d4526e567db
Nana Adjei Manu <claeusdev@gmail.com>
2017-09-27 02:18:33 +0000

    Setup waypoints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,98 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'FittsApp';
+  // Setting zoom level
+  zoom: number = 5;
+
+  // Starting point in the middle of Norway
+  lat: number = 59.911491;
+  lng: number = 10.757933;
+
+  // Setting Variables for waypoint addition
+  waypointName:string;
+  waypointLat:string;
+  waypointLng:string;
+  waypointDraggable:string;
+
+  // Drivers
+  drivers: [
+    {
+      name: 'Jason Aldean',
+      VehicleType: 'Lamboghini',
+      Age: '32'
+    },
+    {
+      name: 'Jake Brown',
+      VehicleType: 'Nissan',
+      Age: '18'
+    }
+  ]
+
+  // Setting Markers
+  waypoints: waypoint[] = [
+    {
+      name: "Company One",
+      lat: 59.911491,
+      lng: 10.757933,
+      draggable: true
+    }
+  ];
+  constructor(){
+
+  }
+
+  clickedWaypoint(waypoint:waypoint,index:number){
+    console.log("Clicked marker: "+waypoint.name+" at index " +index)
+  }
+
+  mapClicked($event:any){
+    var newWaypoint = {
+      name: 'Untitled',
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable:false
+    }
+
+    this.waypoints.push(newWaypoint);
+  }
+
+  waypointDragEnd(waypoint:any, $event:any){
+    var updatedWaypoint = {
+      name: waypoint.name,
+      lat: parseFloat(waypoint.lat),
+      lng: parseFloat(waypoint.lng),
+      draggable: false
+    }
+
+    var newLat = $event.coords.lat;
+    var newLng = $event.coords.lng;
+  }
+
+  addWaypoint() {
+    if(this.waypointDraggable == 'yes'){
+      var isDraggable = true;
+    }else {
+      var isDraggable = false;
+    }
+
+    var newWaypoint = {
+      name: this.waypointName,
+      lat: parseFloat(this.waypointLat),
+      lng: parseFloat(this.waypointLng),
+      draggable: isDraggable
+    }
+
+    this.waypoints.push(newWaypoint);
+  }
+}
+
+// Setting Marker types
+interface waypoint {
+  name: string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAJBeqWoC--NCzDPh7hg6Uzr5ttpVxCgb0'
+    }),
+    AgmSnazzyInfoWindowModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
